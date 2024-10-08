ece99de9cf63359eb049c0e50ab2cc578233ef68
Sean Treacy <seantreacy@campus-009-092.ucdavis.edu>
2024-10-08 14:12:18 -0700

    week 2 homework

---
+++ scripts/week_2_homework.R
@@ -0,0 +1,25 @@
+set.seed(15)
+hw2 <- runif(50, 4, 50) 
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+
+#now we call the object and return all the values that are not NA
+hw2[!is.na(hw2)]
+
+#naming that noNA (???? can't do this?)
+noNA <- hw2[!is.na(hw2)]
+
+#assigning values from 14-38 inclusive to vector prob1
+prob1 <- noNA [noNA>=14 & noNA<=38]
+
+#multiplying prob1 by 3 and assigning that to vector times3
+times3 <- prob1*3
+
+#adding 10 to times3 vector and assigning that to vector plus10 
+plus10 <- times3+10
+
+#returning every other number in plus10 vector, starting with the first one, and assigning them to vector final 
+final <- plus10[c(TRUE,FALSE)]
+
+#printing final 
+final
