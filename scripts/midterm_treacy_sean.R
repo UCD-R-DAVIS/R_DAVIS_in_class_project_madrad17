0b77513c1040b0f11758920eddabc09763205086
Sean Treacy <seantreacy@Seans-MBP-2.attlocal.net>
2024-11-02 16:34:58 -0700

    Went through the answers and coded a second attempt to get the muscle memory down.

--- scripts/midterm_treacy_sean.R
+++ scripts/midterm_treacy_sean.R
@@ -1,5 +1,8 @@
 # Midterm
 
+#Notes: I still don't totally understand what the group_by combined with summarize 
+# functions do
+
 library(tidyverse)
 tyler <- read_csv("https://raw.githubusercontent.com/ucd-cepb/R-DAVIS/refs/heads/main/data/tyler_activity_laps_10-24.csv")
 
@@ -36,3 +39,51 @@ tyler_stats <- tyler_runs %>%
 
 # Tried to troubleshoot for a while and then gave up. I think I did something 
 # wrong when I initally created the pace and form columns.
+
+
+
+
+
+# Second attempt, after looking over the answers, to get the muscle memory down
+
+library(tidyverse)
+ty <- read_csv("https://raw.githubusercontent.com/ucd-cepb/R-DAVIS/refs/heads/main/data/tyler_activity_laps_10-24.csv")
+run <- ty %>% 
+  filter(sport == 'running') %>% 
+  filter(minutes_per_mile < 10 & minutes_per_mile >5) %>% 
+  filter(total_elapsed_time_s > 1) %>% 
+  mutate(
+    pace = case_when(
+      minutes_per_mile < 6 ~ "fast",
+      minutes_per_mile > 6 & minutes_per_mile < 8 ~ "medium",
+      TRUE ~ "slow"),
+    form = case_when(
+      year == 2024 ~ 'new',
+      TRUE ~ 'old'
+    ))
+
+form_at_pace <- run %>% 
+  group_by(form, pace) %>% 
+  summarize(avg_spm = mean(steps_per_minute)) %>% 
+  pivot_wider(id_cols = form, values_from = avg_spm, names_from = pace) %>% 
+  select(form, slow, medium, fast)
+  
+progress_check <- run %>% 
+  filter(form == 'new') %>% 
+  mutate(
+    progress_period = ifelse(month %in% 1:6, 'early 2024', 'late 2024')) %>% 
+  group_by(progress_period) %>% 
+  summarize(
+    min_spm = min(steps_per_minute),
+    median_spm = median(steps_per_minute),
+    mean_spm = mean(steps_per_minute),
+    max_spm = max(steps_per_minute)
+  )
+
+progress_check
+
+#tyler is doing better! 
+
+# I still don't totally understand what the group_by combined with summarize 
+# functions do
+
