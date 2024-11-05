839fc87f863a2c4238e6a2bb1745d225bfae4532
Sean Treacy <seantreacy@Seans-MacBook-Pro-2.local>
2024-11-05 10:44:37 -0800

    Week 6 homework

--- scripts/week_6_homework.R
+++ scripts/week_6_homework.R
@@ -1,4 +1,62 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Week 6 Homework
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+library(tidyverse)
+
+gapminder <- read_csv("https://ucd-r-davis.github.io/R-DAVIS/data/gapminder.csv")
+
+# Mean life expectancy on each continent, and then how life expectancy has 
+# changed over time by continent
+
+gapminder %>% 
+  group_by(continent) %>% 
+  summarise(mean_lifeExp = mean(lifeExp)) %>% 
+  ungroup() %>% 
+  ggplot(data = gapminder, mapping = aes(x = year, y = lifeExp, color = continent))+
+    geom_smooth(aes(color = continent)) +
+    labs(y = "Life Expectancy", x = "Year", color = "Continent") +
+    theme_clean()
+  
+
+# What is scale_x_log10() achieving? What about geom_smooth()?
+
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
+  #size points in proportion to country population
+  geom_point(aes(color = continent, size = pop)) + 
+  scale_x_log10() +
+  #puts it on a log10 scale so that the distribution is more even
+  geom_smooth(method = 'lm', color = 'black', linetype = 'dashed') +
+  #inserts a linear regression line per the specified characteristics
+  theme_bw()
+
+
+#boxplot of life expectency in Brazil, China, El Salvador, Niger, and the US
+
+five_countries <- gapminder %>% filter(
+  country == 'Brazil' | 
+  country == 'China' | 
+  country == 'El Salvador' | 
+  country == 'Niger' |
+  country == 'United States'
+)
+  ggplot(data = five_countries, aes(x = country, y = lifeExp)) + 
+    geom_boxplot(aes(color = country)) +
+    geom_jitter(aes(alpha = 1/100)) + 
+    labs(y = "Life Expectancy", x = "Country", color = "Country", 
+         title = 'Life Expectancy of Five Countries', 
+         subtitle = 'Mean distribution from 1952 - 2007') +
+    guides(alpha = FALSE, color = FALSE) +
+    theme_fivethirtyeight()
+    
+# alpha of the geom_jitter doesn't seem to change point opacity
+  
+view(five_countries)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Week 6 notes
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 library(tidyverse)
 
