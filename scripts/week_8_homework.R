ad23bf7392c9d93b144cb3aa7db6fa7776b0f17e
Sean Treacy <seantreacy@Seans-MBP-2.hsd1.ca.comcast.net>
2024-11-18 15:41:34 -0800

    Week 8 homework progress

--- scripts/week_8_homework.R
+++ scripts/week_8_homework.R
@@ -1,8 +1,13 @@
 library(tidyverse)
-mloa <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
+library(lubridate)
+mloa1 <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
+mloa <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv",
+                 col_types = "cccccccddddddddd")
 
 # Timezone UTC
-
+view(mloa)
+head(mloa)
+glimpse(mloa1)
 # Remove missing values from the following: 
 # 7: WIND SPEED -999.9
 # 10: TEMPERATURE at 2 Meters -999.9
@@ -12,13 +17,33 @@ mloa <- mloa %>% filter(
   windSpeed_m_s != -999.9 &
   temp_C_2m != -999.9 &
   rel_humid != -99
-) %>% 
-# Generate a column called “datetime” using the year, month, day, hour24, and min columns.
-  mutate(
-    
-  )
+)
+
+
+# Generate a column called “datetime” using the year, month, day, hour24, 
+# and min columns.
 # Create a column called “datetimeLocal” that converts the datetime column to 
 # Pacific/Honolulu time (HINT: look at the lubridate function called with_tz()). 
+
+mloa$datetime <- paste(mloa$year, "-", mloa$month,
+                        "-", mloa$day, ", ", mloa$hour24, ":",
+                        mloa$min, sep = "")
+
+mloa$datetime <- ymd_hm(as.character(mloa$datetime))
+                        
+tz(mloa$datetimeLocal)
+
+mloa$datetimeLocal <- with_tz(mloa$datetime, tzone = "Pacific/Honolulu")
+
+mloa$datetimeLocal
+
+months <- month(mloa$datetimeLocal)
+
+table(months)
 # Then, use dplyr to calculate the mean hourly temperature each month using the 
 # temp_C_2m column and the datetimeLocal columns. (HINT: Look at the lubridate 
 # functions called month() and hour()).
+
+
+
+
