9f9c72e183c9990b139a9fe194d87a2387568116
Sean Treacy <seantreacy@Seans-MBP-2.attlocal.net>
2024-11-20 19:05:59 -0800

    Wk 8 hw

--- scripts/week_8_homework.R
+++ scripts/week_8_homework.R
@@ -5,7 +5,7 @@ mloa <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/
                  col_types = "cccccccddddddddd")
 
 # Timezone UTC
-view(mloa)
+view(mloa) 
 head(mloa)
 glimpse(mloa1)
 # Remove missing values from the following: 
@@ -40,10 +40,27 @@ mloa$datetimeLocal
 months <- month(mloa$datetimeLocal)
 
 table(months)
+
 # Then, use dplyr to calculate the mean hourly temperature each month using the 
 # temp_C_2m column and the datetimeLocal columns. (HINT: Look at the lubridate 
 # functions called month() and hour()).
 
+?month()
+?hour()
 
+meanTemp_Month <- mloa %>% 
+  mutate(month_name = month(datetimeLocal, label = TRUE, abbr = FALSE)) %>% 
+  group_by(month_name, hour24) %>% 
+  summarize(meanTemp = mean(temp_C_2m))
+
+view(meanTemp_Month)
+
+hourLocal <- hour(mloa$datetimeLocal) %>% 
+  
+
+length(hourLocal)
+
+ggplot(data = meanTemp_Month, mapping = aes(x = month_name, y = meanTemp)) +
+  geom_point(aes(color = hour24)
 
 
